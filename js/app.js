9e3e3d0c310fca30cbc60e9359817822d86ab7d1
Pedro Miguel <pedro.miguel.e.idk@gmail.com>
2021-08-16 18:32:26 -0300

    Middle Part Fixed

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//Button shows Dialogg
+
 const but = document.querySelector('.hamburguer');
 const menu = document.querySelector('.navbar-links');
 but.addEventListener('click', function aparece(e){
@@ -5,3 +7,25 @@ but.addEventListener('click', function aparece(e){
     menu.classList.toggle("active");
     
 })
+
+//Intersectional 
+const nav = document.querySelector(".navbar");
+const papelDeParede = document.querySelector(".sectionMain");
+
+const papelDeParedeOptions = {
+    rootMargin: "-300px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver){
+    entries.forEach(entry => {
+        //console.log(entry.target);
+        if (!entry.isIntersecting) {
+            nav.classList.add("navScrolled");
+        } else{
+            nav.classList.remove("navScrolled");
+        }
+    })
+}, papelDeParedeOptions);
+
+sectionOneObserver.observe(papelDeParede);
+
